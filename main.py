8dd4e31d85e3b23b1dc0fd9d97265fd95dc1d85a
Byron.C <byronrwcor@protonmail.com>
2024-02-28 18:35:59 -0800

    Forgor to uncomment

--- main.py
+++ main.py
@@ -12,32 +12,32 @@ if __name__ == "__main__":
     DeviceList = open("Device List.txt", "w")
     DeviceList.writelines((deviceNumber, "||", localIP))
     DeviceList.close()
-    # try:
-    #     print("Installing Veyon...")
-    #     configPath = os.path.abspath("ClientConfig.json")
-    #     veyonSetup = "veyon-4.8.2.0-win64-setup.exe"
-    #     args = "/S /NoMaster /ApplyConfig=%cd%\ClientConfig.json"
-    #     subprocess.run([veyonSetup, args], shell=True)
-    # except:
-    #     print("Veyon Install Failed...")
-    # else:
-    #     print("Veyon Install Finished...")
+    try:
+        print("Installing Veyon...")
+        configPath = os.path.abspath("ClientConfig.json")
+        veyonSetup = "veyon-4.8.2.0-win64-setup.exe"
+        args = "/S /NoMaster /ApplyConfig=%cd%\ClientConfig.json"
+        subprocess.run([veyonSetup, args], shell=True)
+    except:
+        print("Veyon Install Failed...")
+    else:
+        print("Veyon Install Finished...")
 
-    # try:
-    #     print("Imporing Auth Keys")
-    #     os.chdir("C:/Program Files/Veyon")
-    #     subprocess.run(["./veyon-wcli", f'authkeys import Dojo/public "{defaultDir}\DojoLaptops_public_key.pem"'])
-    # except:
-    #     print("Auth Key Import Failed...\n")
-    # else:
-    #     print("Auth Key Import Success...\n")
+    try:
+        print("Imporing Auth Keys")
+        os.chdir("C:/Program Files/Veyon")
+        subprocess.run(["./veyon-wcli", f'authkeys import Dojo/public "{defaultDir}\DojoLaptops_public_key.pem"'])
+    except:
+        print("Auth Key Import Failed...\n")
+    else:
+        print("Auth Key Import Success...\n")
 
-    # try:
-    #     print("Setting Auth Key Group")
-    #     subprocess.run(["./veyon-wcli", "authkeys setaccessgroup Dojo/public Users"])
-    # except:
-    #     print("Setting Key Group Failed...")
-    # else:
-    #     print("Setting Auth Key Group Success...")
+    try:
+        print("Setting Auth Key Group")
+        subprocess.run(["./veyon-wcli", "authkeys setaccessgroup Dojo/public Users"])
+    except:
+        print("Setting Key Group Failed...")
+    else:
+        print("Setting Auth Key Group Success...")
 
-    # input("\nPress Key to Finish...\n")
+    input("\nPress Key to Finish...\n")
